1a186e638153c3914cc85d7b330acdf32dcf2fa0
Huaiku <banboll@outlook.com>
2018-11-13 18:35:21 +0800

    add old project zip file

---
+++ src/main/java/com/huaiku/jlearn/JLearn.java
@@ -0,0 +1,15 @@
+package com.huaiku.jlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class JLearn {
+	public static void main(String[] args) {
+		SpringApplication.run(JLearn.class, args);
+	}
+}
