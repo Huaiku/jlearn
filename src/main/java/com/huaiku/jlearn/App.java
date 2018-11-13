934cab5015eb688ee2e6a783202d9f9407bde028
Huaiku <banboll@outlook.com>
2018-11-13 16:02:06 +0800

    init project

---
+++ src/main/java/com/huaiku/jlearn/App.java
@@ -0,0 +1,13 @@
+package com.huaiku.jlearn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
